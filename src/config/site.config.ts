b77e16ec445adc2162f274b8b87c0811bd0f3508
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-11 11:27:14 +0600

    init site config

---
+++ src/config/site.config.ts
@@ -0,0 +1,29 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "NSC Starter",
+  title: "Next.js ShadCN Starter - A Next.js 14 Starter with Tailwind CSS, ShadCN and TypeScript",
+  description:
+    "A Next.js 13/14 Starter with Tailwind CSS, ShadCN and TypeScript",
+  keywords: [
+    "Next.js",
+    "Next.js 13",
+    "Next.js 14",
+    "Tailwind CSS",
+    "ShadCN",
+    "TypeScript",
+    "Starter",
+    "Template",
+    "Boilerplate",
+  ],
+  creator: {
+    name: "rds_agi",
+    url: "https://rdsx.dev",
+  },
+  siteUrl: "https://nsc-starter.vercel.app",
+  ogImage: "https://nsc-starter.vercel.app/og.png",
+  links: {
+    x: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip/nsc-starter",
+  },
+};
