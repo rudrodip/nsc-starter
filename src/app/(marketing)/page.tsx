8fb3773977c023ac812a83553a1efbb9f8d9c0c2
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-11 11:25:20 +0600

    updated

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-between py-12">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex p-2">
+        <p className="flex w-full p-2 justify-center pb-6 pt-8 lg:w-auto rounded-xl border">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/(marketing)/page.tsx</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+        <div className="flex w-full items-end justify-center lg:static lg:size-auto lg:bg-none">
           <p
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
           >
